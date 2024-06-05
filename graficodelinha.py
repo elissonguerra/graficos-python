1ba1db3acf514a5b8a92cf55f86216f9f3dae632
elissonguerra <elisson.guerra@gmail.com>
2024-06-05 20:15:00 -0300

    Adicionado 4 tipos de gráficos

---
+++ graficodelinha.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# Dados para o eixo x e o eixo y
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+#Criar o gráfico de linha
+plt.plot(x, y)
+
+#Adicionar rótulos aos eixos
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+# Adicionar um título
+plt.title('Meu Gráfico de Linha')
+
+# Exibir o gráfico
+plt.show()
